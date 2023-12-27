9e6d3fd67a736c90a5be9bd48964e554179be035
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-12-28 00:04:01 +0530

    last commit

--- Routes/Products.js
+++ Routes/Products.js
@@ -182,4 +182,40 @@ router.delete("/products/:productId", userAuth, async (req, res) => {
   }
 });
 
+
+// Route for filtering products
+router.get('/products/filter', async (req, res) => {
+  try {
+    const { category, subcategory, priceMin, priceMax, location } = req.query;
+
+    const filters = {};
+
+    if (category) {
+      filters.category = category;
+    }
+
+    if (subcategory) {
+      filters.subcategory = subcategory;
+    }
+
+    if (priceMin && priceMax) {
+      filters.price = { $gte: priceMin, $lte: priceMax };
+    } else if (priceMin) {
+      filters.price = { $gte: priceMin };
+    } else if (priceMax) {
+      filters.price = { $lte: priceMax };
+    }
+
+    if (location) {
+      filters.location = location;
+    }
+
+    const products = await Product.find(filters).populate('category').populate('subcategory');
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+
 module.exports = router;
